bb63c2ff4f44b2cab91ec22ecdfc8b4d6c583f18
DanStudio-Daniel <danielmojar26@gmail.com>
2026-05-14 19:03:24 +0800

    Update devs.ts

--- CommandFiles/commands/devs.ts
+++ CommandFiles/commands/devs.ts
@@ -3,7 +3,7 @@ import { defineEntry } from "@cass/define";
 
 export const meta: CassidySpectra.CommandMeta = {
   name: "devs",
-  aliases: ["dan", "azuki", "yuan", "zyro", "zy"],
+  aliases: ["dan", "azuki", "yuan", "zyro", "zy", "Caspian", "Rainer", "ashtralyn", "Jhanica" , "Izumi", "aziana", "Ericka", "Hershey", "Mharco", "Ladylyn", "Ruzzel", "Anthony"],
   description: "Asaran para sa mga devs.",
   version: "1.1.0",
   author: "AzukiDan",
@@ -21,16 +21,16 @@ const configs: Config[] = [
     async handler({ output }) {
       const responses = [
         "busy sya kaka bebetime.",
-        "naga lu² pa sya, 'wag mo muna gambalahin ang ritwal.",
+        "naga pabb pa sya sa jowa nya.",
         "patay na yun, 'wag mo na hanapin. Ipag-tira mo na lang ng bulaklak.",
         "bat hanap mo? crush mo no? aminin mo na, 'wag nang pabebe.",
         "puyat kaka lose streak.",
-        "puyat kaka nood bold.",
+        "doon pa sya sa ere, iniwan.",
         "naka-offline.",
         "busy mag-isip kung paano maging gwapo.",
         "broken sya wag mo istorbohin.",
         "naglalaro lang ng Roblox 'yan, doon daw mas may progress utak nya.",
-        "nag lulu pa ng saging yun.",
+        "yun lumilipad.",
         "naghahanap ng motivation, pero ang totoo tamad lang talaga.",
         "naka-disable ang utak ngayon, try again next year.",
         "ka-chat yung AI, doon lang daw sya hindi nire-reject.",
